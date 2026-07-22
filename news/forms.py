8b73bc149e30834be5f1f41fd714b15970371e45
MykolaLozanDevelopment <mykola.lozan.development@gmail.com>
2026-07-22 13:27:54 +0300

    Add RedactorCreationForm, NewspaperForm and search forms

---
+++ news/forms.py
@@ -0,0 +1,62 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+from .models import Newspaper, Redactor
+
+
+class RedactorCreationForm(UserCreationForm):
+    class Meta:
+        model = Redactor
+        fields = (
+            "username",
+            "years_of_experience",
+            "first_name", "last_name"
+        )
+
+
+class RedactorYearsOfExperienceUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Redactor
+        fields = ["years_of_experience"]
+
+
+class NewspaperForm(forms.ModelForm):
+    publishers = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Newspaper
+        fields = "__all__"
+        widgets = {
+            "published_date": forms.DateInput(attrs={"type": "date"}),
+        }
+
+
+class NewspaperSearchForm(forms.Form):
+    title = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by title"}),
+    )
+
+
+class RedactorSearchForm(forms.Form):
+    username = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by username"}),
+    )
+
+
+class TopicSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by name"}),
+    )
