84772832df96995358f19dbccb43ce7278aa537c
MykolaLozanDevelopment <mykola.lozan.development@gmail.com>
2026-07-22 11:50:12 +0300

    Register models in admin panel, create Superuser

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import  Topic, Redactor, Newspaper
+
+
+admin.site.register(Topic)
+
+
+@admin.register(Redactor)
+class RedactorAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("years_of_experience", )}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Additional info",
+            {"fields": ("years_of_experience", "first_name", "last_name")},
+        ),
+    )
+
+
+@admin.register(Newspaper)
+class NewspaperAdmin(admin.ModelAdmin):
+    list_display = ("title", "topic", "published_date")
+    filter_horizontal = ("publishers",)
